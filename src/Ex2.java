4ed87148aeacacb0429ded345a93c5cf08cd3976
munoz.rocosa.arnaut <munoz.rocosa.arnau@alumnat.copernic.cat>
2023-03-07 20:45:31 +0100

    Date validation

--- src/Ex2.java
+++ src/Ex2.java
@@ -3,12 +3,31 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /*
- *   Input: "12:30pm-12:00am"
+ *   Input: "11:00pm-12:00pm"
  *   Output: 690
  */
 public class Ex2 {
 
     public static String StringChallenge(String str) {
+        //hour = {"hour1","hour2"}
+        //hours = {(hour1:"hour","mins","am/pm"),(hour2:"hour","mins","am/pm")}          10:59pm -> 12:58pm
+        String[] hour = str.split("-");
+        String[][] hours = new String[2][3];
+
+        for (int i = 0; i < hour.length; i++) {
+            if (hour[i].length() < 7) {
+                hours[i][0] = hour[i].substring(0, 1);
+                hours[i][1] = hour[i].substring(2, 3);
+                hours[i][2] = hour[i].substring(4, 6);
+            } else {
+                hours[i][0] = hour[i].substring(0, 2);
+                hours[i][1] = hour[i].substring(3, 5);
+                hours[i][2] = hour[i].substring(5, 7);
+            }
+        }
+        System.out.println("Hora 1--->Hora: " + hours[0][0] + " Minutos: " + hours[0][1] + " Time: " + hours[0][2]);
+        System.out.println("Hora 2--->Hora: " + hours[1][0] + " Minutos: " + hours[1][1] + " Time: " + hours[1][2]);
+
 
         return str;
     }
@@ -19,12 +38,11 @@ public class Ex2 {
 
         if (!str.isEmpty()) {
             if (m.matches()) {
-                System.out.println("String matches!");
                 return true;
             } else {
-                System.out.println("Not a valid input\n-----------------\nValid input example:\n'12:30pm-12:00am'\n");
+                System.out.println("Not a valid input\n--------------------\nValid input example:\n'12:30pm-12:00am'\n'1:00pm-1:45pm'\n'11:30pm-12:00am'\n");
             }
-        } else{
+        } else {
             System.out.println("Empty input");
         }
         return false;
@@ -41,5 +59,10 @@ public class Ex2 {
             System.out.print("Input: ");
             str = s.nextLine();
         }
+
+        System.out.println(StringChallenge(str));
+
     }
 }
+
+
